5f4a5906d7a450f0c2a08d8e9da770a27164a36c
mosh <nikajamaspishvili@icloud.com>
2023-04-17 20:36:55 +0400

    calculatorsaved

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,59 @@
-import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import { Search } from './components/search';
+import { useState } from "react";
 import "./App.css";
-import {QueryClient,QueryClientProvider} from "@tanstack/react-query";
-import {Form} from "./components/form";
 
-export function App(){
-    const query=new QueryClient();
+export function App() {
+  const [input, setInput] = useState("");
+const [boolean,setBoolean]=useState(true);
+
+
+  const handleClick = (value: string) => {
+    setBoolean(true);
+    if(boolean){
+            setInput((prevInput) => prevInput + value);
+        
+    }else{
+     setInput('');
+    }
+   
+  };
+
+  const calculate = () => {
+    const result = eval(input);
+    setInput(result.toString());
+     setBoolean(false);
+  };
+  const clear=()=>{
+    setInput('');
+  }
+
+
+
   return (
-   <div>
-    <QueryClientProvider client={query}>
-    <Router>
-        <Routes>
-            <Route path='/home' element={<Search />}/>
-            <Route path="/" element={<Form />}/>
-        </Routes>
-    </Router>
-    </QueryClientProvider>
-   </div>
+    <div className="calculator_div">
+      <div className="box" onClick={() => handleClick("1")}>1</div>
+      <div className="box" onClick={() => handleClick("2")}>2</div>
+      <div className="box" onClick={() => handleClick("3")}>3</div>
+      <div className="box" onClick={() => handleClick("4")}>4</div>
+      <div className="box" onClick={() => handleClick("5")}>5</div>
+      <div className="box" onClick={() => handleClick("6")}>6</div>
+      <div className="box" onClick={() => handleClick("7")}>7</div>
+      <div className="box" onClick={() => handleClick("8")}>8</div>
+      <div className="box" onClick={() => handleClick("9")}>9</div>
+      <div className="plus" onClick={()=>{input && boolean ? setInput((input)=>input +'+'):null}}>+</div>
+      <div className="equal" onClick={calculate}>=</div>
+      <div className="multiply" onClick={()=>{{input && boolean? setInput((input)=>input +'*'):null}}}>x</div>
+      <div className="minus" onClick={()=>{input && boolean? setInput((input)=>input +'-'):null}}>-</div>
+      <div className="clear" onClick={clear}>C</div>
+
+      <Result input={input} />
+    </div>
   );
 }
+
+const Result = (props: { input: string }) => {
+  return (
+    <div className="search">
+      <div className="results">{props.input}</div>
+    </div>
+  );
+};
